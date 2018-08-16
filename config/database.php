bfe045638e05d3df771d09ed3e4cf050a709e658
haidi nurhadinata <nhaidii@yahoo.com>
2018-08-16 11:44:07 +0800

    update

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     */
 
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'heroku'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,6 +52,17 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
+        ],
+
+        'heroku' => [
+            'driver'   => 'pgsql',
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
         ],
 
         'sqlsrv' => [
